6b4fac6279a9fbb926fedb391ae7d809548022bb
Marina Ponomar <mponomar96@gmail.com>
2026-05-28 15:47:51 +0200

    Solution4

--- src/components/CommentInfo/CommentInfo.tsx
+++ src/components/CommentInfo/CommentInfo.tsx
@@ -9,7 +9,10 @@ export const CommentInfo: React.FC<Props> = ({ comment }) => (
   <div className="CommentInfo">
     <div className="CommentInfo__title">
       <strong className="CommentInfo__name">{comment.name}</strong>
-      {` by ${comment.email}`}
+      {` by `}
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
     </div>
     <div className="CommentInfo__body">{comment.body}</div>
   </div>
--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Post } from '../../types/Post';
 import { CommentList } from '../CommentList/CommentList';
+import { UserInfo } from '../UserInfo/UserInfo';
 
 type Props = {
   post: Post;
@@ -11,7 +12,10 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
 
-      <p>{`Posted by ${post.user?.name}`}</p>
+      <p>
+        {`Posted by `}
+        {post.user && <UserInfo user={post.user} />}
+      </p>
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
